be5f65cf528b606c7e413ee44c9195f4e0216895
Emmanuel <amprebonne@gmail.com>
2023-10-01 22:25:39 -0700

    created 2-strlent_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/*
+ * int _strlen_recursion(char *s) - returns the length of a string
+ * 's': the string passed
+ * Return: on success, length of string
+ * on error, -1 is returned
+ */
+
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s)
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
+
+	return (length);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,3 +1,3 @@
 0. void _puts_recursion(char *s); -  a function that prints a string, followed by a new line
 1. void _print_rev_recursion(char *s); - a function that prints a string in reverse
-2. 
+2. int _strlen_recursion(char *s); - a function that returns the length of a string 
